4d89bf3db312c832af8e52602486ca75a74f5bb5
Mr Rajput <daksharajkamal111@gmail.com>
2020-11-07 15:42:18 +0530

    checkout page completed

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,22 +1,29 @@
 import React from "react";
 import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal";
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
         <img src="" alt="" className="checkout__ad  " />
         <div>
           <h2 className="checkout__title">Your Shopping Basket</h2>
-          {/* BasketItem */}
-          {/* BasketItem */}
-          {/* BasketItem */}
-          {/* BasketItem */}
-          {/* BasketItem */}
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
-      <div className="checkout__right"> 
+      <div className="checkout__right">
         <Subtotal />
       </div>
     </div>
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -2,18 +2,18 @@ import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "./StateProvider";
-import { gotBasketTotal } from "./reducer";
+import { getBasketTotal } from "./reducer";
 
 function Subtotal() {
   const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="subtotal">
       <CurrencyFormat
-        renderText={(Value) => (
+        renderText={(value) => (
           <>
             <p>
-              Subtotal ({basket.length}items):
-              <strong>('₹ {basket.value}')</strong>
+              Subtotal ({basket.length} items):
+              <strong> {value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" />
@@ -22,7 +22,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={gotBasketTotal(basket)}
+        value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSeprator={true}
         prefix={"₹"}
