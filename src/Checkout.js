44a0add57ed39bc1222b0ac1dd88838c044bc203
Mr Rajput <daksharajkamal111@gmail.com>
2020-11-10 19:46:06 +0530

    First Deployment Done with link 'https://fir-clone-d52ea.web.app/'

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,12 +5,13 @@ import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal";
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
         <img src="" alt="" className="checkout__ad  " />
         <div>
+          <h3>Hello, {user?.email}</h3>
           <h2 className="checkout__title">Your Shopping Basket</h2>
           {basket.map((item) => (
             <CheckoutProduct
